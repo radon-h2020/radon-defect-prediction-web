a2658c6e0e557fd400f7158323260dcc83c1a175
stefanodallapalma <stefano.dallapalma0@gmail.com>
2019-09-18 18:23:12 +0200

    interfaced with api

---
+++ js/upload-and-predict.js
@@ -0,0 +1,159 @@
++ function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+    var steps = 0
+    var fileCount = 0
+
+    var initializeProgressbar = function(){
+        steps = 0
+        $("#progressbar-upload").attr('aria-valuenow', 0).css('width', '0%');
+        $("#progressbar-status").html('0% Complete');
+    }
+
+    var uploadProgressbar = function(value){
+        steps += 1
+        value = Math.ceil(value*steps);
+        $("#progressbar-upload").attr('aria-valuenow', value).css('width', value.toString()+'%');
+        $("#progressbar-status").html(value.toString()+ '% Complete');
+    }
+
+    var startUpload = function(files) {
+        initializeProgressbar()
+        let step = parseFloat(100 / files.length)
+        
+        for(let i=0; i < files.length; i++){
+            
+            if (files[i]) { 
+                var reader = new FileReader();
+                reader.readAsText(files[i], "UTF-8");
+
+                reader.onload = function (evt) {
+                  
+                    $.ajax({
+                        url: 'http://localhost:5000/api/classification/classify',      //TODO change address
+                        method:'POST',
+                        type:'POST',
+                        contentType: 'text/plain',
+                        dataType: 'json', // Tell jQuery what kind of response to expect
+                        data: evt.target.result.toString(),
+                    })
+                    .done(function(response) {
+                        uploadProgressbar(step)
+                        let warningOrSuccess = response.defective ? 'warning' : 'success' 
+                        let alert = response.defective ? 'alert-warning' : 'alert-success' 
+                        
+                        let a = $('<a href="#" class="list-group-item list-group-item-'+warningOrSuccess+'"><span class="badge '+ alert + ' pull-right">'+ (++fileCount) +'</span> '+files[i].name+' </a>')
+                        a.data('content', evt.target.result.toString())
+                        a.data('metrics', response.metrics)
+                        a.click(function(){
+                            console.log(JSON.stringify($(this).data().metrics))
+                            
+                            $("#predictionInfoModal").modal();
+                            
+                            // Adjust data for table
+
+                            
+                            // Populate table
+                            var table = $('#dataTable').DataTable({
+                                data: setupDataForTable($(this).data().metrics),
+                                columns : [{
+                                    "data" : "name"
+                                }, {
+                                    "data" : "value"
+                                },{
+                                    "data" : null, 
+                                    render: function ( data, type, row ) {
+                                        return '<input type="checkbox"></input>'
+                                    }
+                                }] 
+                            });
+
+                            $("#file-content").html($(this).data().content);
+                            
+                        });
+
+                        let status = $('<span class="badge '+ alert +' pull-right" style="float: right; margin-right: 10px"> Defective: '+ response.defective.toString().toUpperCase()+'</span> ')
+                        let x = $('<i class="fas fa-trash" style="text-align: center; float: right"></i>')
+                        x.click(function(e){
+                            a.remove();
+                        });
+
+                        a.append(x, status)
+                        $("#processed-files-list").append(a)                        
+                    })
+                    .fail(function () {
+                        uploadProgressbar((i+1)*step)
+                        
+                        let a = $('<a href="#" class="list-group-item list-group-item-danger"><span class="badge alert-danger pull-right">'+ (++fileCount) +'</span> '+files[i].name+' </a>')
+                        let x = $('<i class="fas fa-trash" style="text-align: center; float: right"></i>')
+                        x.click(function(){
+                            a.remove();
+                        });
+
+                        a.append(x)
+                        $("#processed-files-list").append(a)
+                    }) 
+                }
+                
+                reader.onerror = function (evt) {
+                    uploadProgressbar((i+1)*step)
+                                            
+                    let a = $('<a href="#" class="list-group-item list-group-item-danger"><span class="badge alert-danger pull-right">'+ (++fileCount) +'</span> '+files[i].name+' </a>')
+                    let x = $('<i class="fas fa-trash" style="text-align: center; float: right"></i>')
+                    x.click(function(){
+                        a.remove();
+                    });
+
+                    a.append(x)
+                    $("#processed-files-list").append(a)
+                }
+            }
+        }
+
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+}(jQuery);
+
+
+function setupDataForTable(metricsData){
+    
+    let json = [];
+
+    for (let key in metricsData) {
+
+        if (metricsData[key].count > 0){
+            json.push({
+                name: key.toUpperCase(),
+                value: metricsData[key].count
+            });
+        }
+    }
+
+    return json;
+}
